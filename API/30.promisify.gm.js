70693d4180a8d8d4bdd40234bcb4e6418605cf06
shangfeiSF <tony20100125@126.com>
2016-04-22 17:40:19 +0800

    comb Promise.promisify()

--- API/30.promisify.gm.js
+++ API/30.promisify.gm.js
@@ -4,35 +4,45 @@ var path = require('path')
 var Promise = require('bluebird')
 var fs = Promise.promisifyAll(require("fs"))
 
-var colors = require('colors')
 var gm = require('gm')
+var colors = require('colors')
 
-var common = require('./00.common')
-
+// gm instance has these getters
 var getters = ["size", "identify", "format", "depth", "color", "res", "filesize"]
 
 function Getters(name) {
   var imagePath = path.join(__dirname, '../asset', name)
-  var context = gm(imagePath)
+  // create the gm instance by imagePath
+  var instance = gm(imagePath)
 
+  // iterate all the getters of instance
   return Promise.reduce(getters, function (census, getter) {
-      var async = Promise.promisify(context[getter], {
-        context: context
+      /* Promise.promisify the getter of instance*/
+      var asyncGetter = Promise.promisify(instance[getter], {
+        // set the context important！
+        context: instance
       })
 
-      return async().then(function (result) {
+      return asyncGetter().then(function (result) {
+        // restore the result of asyncGetter into census
         census[getter] = result
         return census
       })
     }, {})
     .then(function (census) {
       var data = JSON.stringify({census: census}, null, 2)
-      return fs.writeFileAsync('../log/30.promisify.gm.json', data, {encoding: 'utf-8', flag: 'w+'}).return(census)
+
+      // log census into json file
+      return fs.writeFileAsync('../log/30.promisify.gm.json', data, {
+        encoding: 'utf-8',
+        flag: 'w+'
+      }).return(census)
     })
 }
 
 new Getters('30.promisify.gm.jpg')
   .then(function (census) {
     var data = JSON.stringify({census: census}, null, 2)
-    console.log((data).green)
+
+    console.log(data.green)
   })
